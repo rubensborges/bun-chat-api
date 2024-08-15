e185c266c453a04b70219f98e2c40f59d984e3ea
Rubens Borges <contact@rubensborges.dev>
2024-08-14 23:32:23 -0300

    feat: created controller for participants

---
+++ src/controllers/participantsController.ts
@@ -0,0 +1,43 @@
+import { participants, type Participant } from "../models/participant";
+import { messages, type Message } from "../models/message";
+import dayjs from "dayjs";
+
+export async function handleAddParticipant(req: Request) {
+  const body = await req.json();
+  const { name } = body;
+
+  if (!name) {
+    return new Response(null, { status: 400 });
+  }
+  const participantAlreadyExists = participants.some(
+    (participant) => participant.name === name
+  );
+
+  if (participantAlreadyExists) {
+    return new Response(null, { status: 400 });
+  }
+
+  const participant: Participant = {
+    name,
+    lastStatus: Date.now(),
+  };
+  participants.push(participant);
+
+  const currentTime = dayjs().format("HH:mm:ss");
+
+  const message: Message = {
+    from: name,
+    to: "Todos",
+    text: "entra na sala...",
+    type: "status",
+    time: currentTime,
+  };
+
+  messages.push(message);
+
+  return new Response("ok", { status: 200 });
+}
+
+export async function handleGetParticipants() {
+  return new Response(JSON.stringify(messages));
+}
